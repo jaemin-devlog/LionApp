939b8eb939a28089a3127d0fc8af16e952c1058e
likelion <jjm0203311@naver.com>
2025-03-28 23:16:52 +0900

    Initial commit

---
+++ src/main/java/com/example/lionapp/LionAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.lionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LionAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lionapp/LionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
